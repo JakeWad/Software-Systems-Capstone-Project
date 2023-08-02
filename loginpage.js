06af79fb9c5fd73a539f734568509fc66bc9a7ec
yo <126212399+JakeWad@users.noreply.github.com>
2023-08-02 11:05:42 -0500

    loginpage.js

---
+++ loginpage.js
@@ -0,0 +1,30 @@
+// Login Page
+
+const loginForm = document.getElementById("login-form");
+const loginEmail = document.getElementById("login-email");
+const loginPassword = document.getElementById("login-password");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  // Validate form
+
+  // Submit form data
+  
+});
+
+// Registration Page 
+
+const registerForm = document.getElementById("register-form");
+const registerName = document.getElementById("register-name"); 
+const registerEmail = document.getElementById("register-email");
+const registerPassword = document.getElementById("register-password");
+
+registerForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Validate form
+
+  // Submit form data
+  
+});
